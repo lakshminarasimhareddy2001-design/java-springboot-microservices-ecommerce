f3507834b7d4560776ad41c062eeb7656c6731d3
Lakshmi Narasimha Reddy <lnreddyreddy405@gmail.com>
2026-03-06 17:22:56 +0530

    adding swagger tag and opertaion in CategoryRestController

---
+++ product-service/src/main/java/com/eswar/productservice/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.eswar.productservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private static final String[] SWAGGER_WHITELIST = {
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**",
+            "/v3/api-docs.yaml",
+            "/v3/api-docs"
+    };
+    private static final String[] ACTUATOR_WHITELIST = {
+            "/actuator/health",
+            "/actuator/info"
+    };
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
+
+
+
+        return  httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(
+                        request -> request
+
+
+                                .requestMatchers(
+                                        SWAGGER_WHITELIST
+                                ).permitAll()
+                                .requestMatchers(
+                                        ACTUATOR_WHITELIST
+                                ).permitAll()
+                                // Public product view
+                                .requestMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll()
+
+                                // Admin only
+                                .requestMatchers(HttpMethod.POST, "/api/v1/products/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.PUT, "/api/v1/products/**").hasRole("ADMIN")
+                                .requestMatchers(HttpMethod.DELETE, "/api/v1/products/**").hasRole("ADMIN")
+
+                                .anyRequest().authenticated()
+                        
+
+                )
+                .build();
+
+
+    }
+}
--- product-service/src/main/java/com/eswar/productservice/rest/CategoryRestController.java
+++ product-service/src/main/java/com/eswar/productservice/rest/CategoryRestController.java
@@ -4,6 +4,7 @@ package com.eswar.productservice.rest;
 import com.eswar.productservice.dto.CategoryRequestDto;
 import com.eswar.productservice.dto.CategoryResponseDto;
 import com.eswar.productservice.service.ICategoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,22 +25,26 @@ public class CategoryRestController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "create Product",description = "to create product in ecommerce")
     public CategoryResponseDto create(
             @Valid @RequestBody CategoryRequestDto request) {
         return service.create(request);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "get Product By Id",description = "using id we need to fetch product")
     public ResponseEntity<CategoryResponseDto> getById(@PathVariable UUID id) {
         return  ResponseEntity.ok(service.getById(id));
     }
 
     @GetMapping
+    @Operation(summary = "get  all Products",description = "fetching all products")
     public ResponseEntity< List<CategoryResponseDto>> getAll() {
         return  ResponseEntity.ok( service.getAll());
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "update Product By Id",description = "using id we need to update product")
     public ResponseEntity<CategoryResponseDto> update(
             @PathVariable UUID id,
             @Valid @RequestBody CategoryRequestDto request) {
@@ -47,6 +52,7 @@ public class CategoryRestController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "delete Product By Id",description = "using id we need to delete product")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable UUID id) {
         service.delete(id);
